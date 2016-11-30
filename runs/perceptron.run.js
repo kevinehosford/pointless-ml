fcb8194fc8f783d58b806dbc078af6bbe1e1b60d
KHosford <khosford@luc.id>
2016-11-29 18:08:58 -0600

    add perceptron run

---
+++ runs/perceptron.run.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const { add, sub, scale, dot, fmt} = require('../src/vector');
+const { train, guess, activate } = require('../src/perceptron');
+
+const trainingData = [
+  [[0,0], 0],
+  [[0,1], 1],
+  [[1,0], 1],
+  [[1,1], 1]
+];
+
+const getRandomElement = arr => {
+    return arr[Math.floor(Math.random() * arr.length)];
+};
+
+const rate = 0.2;
+let weights = [
+    Math.random(),
+    Math.random(),
+];
+
+const iterations = 100;
+
+for (let i = 0; i < iterations; i++) {
+    let [input, output] = getRandomElement(trainingData);
+    weights = train(rate, weights, input, output);
+}
+
+console.log(fmt(weights));
+
+for (let [input] of trainingData) {
+    let result = guess(weights, input);
+    console.log(`${fmt(input)} => ${result} => ${activate(result)}`);
+}
